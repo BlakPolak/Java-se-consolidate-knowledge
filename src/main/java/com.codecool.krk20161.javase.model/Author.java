db6a596b29e846754af453239e9076b6edbe5ce6
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-08 19:00:54 +0200

    Create constructors, getters and setters

--- src/main/java/com.codecool.krk20161.javase.model/Author.java
+++ src/main/java/com.codecool.krk20161.javase.model/Author.java
@@ -2,4 +2,28 @@ package com.codecool.krk20161.javase.model;
 
 
 public class Author {
+
+    private String name;
+    private String biography;
+
+    public Author(String name, String biography) {
+        this.name = name;
+        this.biography = biography;
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
 }
--- src/main/java/com.codecool.krk20161.javase.model/Book.java
+++ src/main/java/com.codecool.krk20161.javase.model/Book.java
@@ -2,4 +2,35 @@ package com.codecool.krk20161.javase.model;
 
 
 public class Book {
+
+    private String title;
+    private final Author author;
+    private String summary;
+
+    public Book(String title, Author author, String summary) {
+        this.title = title;
+        this.author = author;
+        this.summary = summary;
+    }
+
+    public Book(String title, Author author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 }
