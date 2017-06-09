bc5e46c4fbf0bd4daa81c8ec7393406865aeead0
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-09 22:25:52 +0200

    Create constructor and getBooks method

--- src/main/java/com.codecool.krk20161.javase.model/Catalog.java
+++ src/main/java/com.codecool.krk20161.javase.model/Catalog.java
@@ -1,5 +1,18 @@
 package com.codecool.krk20161.javase.model;
 
 
+import java.util.ArrayList;
+
 public class Catalog {
+
+    private ArrayList<Book> books;
+
+    public Catalog(){
+    }
+    
+    public ArrayList<Book> getBooks() {
+        return books;
+    }
+
+
 }
