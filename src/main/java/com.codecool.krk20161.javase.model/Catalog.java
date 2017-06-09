e37392f9dfb10b43b916f3de63e8cb2a5d872d30
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-09 22:55:35 +0200

    Implement add method

--- src/main/java/com.codecool.krk20161.javase.model/Catalog.java
+++ src/main/java/com.codecool.krk20161.javase.model/Catalog.java
@@ -1,7 +1,10 @@
 package com.codecool.krk20161.javase.model;
 
 
+import com.codecool.krk20161.javase.exception.AlreadyInCatalogException;
+
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Catalog {
 
@@ -13,7 +16,19 @@ public class Catalog {
     public ArrayList<Book> getBooks() {
         return books;
     }
-    public void add(Book book) {
-    }
 
+    public void add(Book book){
+        try{
+            Iterator catalogIterator = books.iterator();
+            while (catalogIterator.hasNext()) {
+                Book bookInCatalog = (Book) catalogIterator.next();
+                if (book.equals(bookInCatalog)){
+                    throw new AlreadyInCatalogException();
+                }
+            }
+            books.add(book);
+        }catch (AlreadyInCatalogException e){
+            e.getMessage("This book is already in catalog, you can not add it");
+        }
+    }
 }
