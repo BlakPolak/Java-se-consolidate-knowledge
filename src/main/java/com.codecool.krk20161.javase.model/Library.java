e722600f24b939ce234da041a37395b1a3ae7827
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-10 00:17:00 +0200

    Create constructor, getter and setter for catalog

--- src/main/java/com.codecool.krk20161.javase.model/Library.java
+++ src/main/java/com.codecool.krk20161.javase.model/Library.java
@@ -2,4 +2,15 @@ package com.codecool.krk20161.javase.model;
 
 
 public class Library {
+    Catalog catalog = new Catalog();
+
+    public Library(){
+    }
+    public Catalog getCatalog() {
+        return catalog;
+    }
+
+    public void setCatalog(Catalog catalog) {
+        this.catalog = catalog;
+    }
 }
