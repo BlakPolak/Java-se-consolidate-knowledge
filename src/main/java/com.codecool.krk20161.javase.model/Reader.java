68cb338cb28fa46befc6db273287307086234fca
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-10 16:14:05 +0200

    Add reader iterator to constructors

--- src/main/java/com.codecool.krk20161.javase.model/Reader.java
+++ src/main/java/com.codecool.krk20161.javase.model/Reader.java
@@ -2,18 +2,21 @@ package com.codecool.krk20161.javase.model;
 
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class Reader implements Search {
     String name;
     List<Book> read = new ArrayList<Book>();
+    Iterator readerIterator;
     public Reader(String name){
         this.name = name;
-
+        this.readerIterator = read.iterator();
     }
     public Reader(String name, List<Book> read) {
         this.name = name;
         this.read = read;
+        this.readerIterator = read.iterator();
     }
 
     public List<Book> getRead() {
