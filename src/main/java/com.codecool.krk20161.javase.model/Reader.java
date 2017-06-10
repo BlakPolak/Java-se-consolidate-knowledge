953c1b941c6e56a278849967a609335edadf1cb0
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-10 16:19:03 +0200

    Deletes iterator and changes while loops to for each loops

--- src/main/java/com.codecool.krk20161.javase.model/Reader.java
+++ src/main/java/com.codecool.krk20161.javase.model/Reader.java
@@ -2,21 +2,18 @@ package com.codecool.krk20161.javase.model;
 
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class Reader implements Search {
     String name;
     List<Book> read = new ArrayList<Book>();
-    Iterator readerIterator;
+
     public Reader(String name){
         this.name = name;
-        this.readerIterator = read.iterator();
     }
     public Reader(String name, List<Book> read) {
         this.name = name;
         this.read = read;
-        this.readerIterator = read.iterator();
     }
 
     public List<Book> getRead() {
@@ -30,11 +27,9 @@ public class Reader implements Search {
     @Override
     public List<Book> searchByTitle(String title) {
         List<Book> searchedBook = new ArrayList<>();
-        while (readerIterator.hasNext()) {
-            Book bookInReader = (Book) readerIterator.next();
-            if (bookInReader.getTitle().equals(title)) {
-                searchedBook.add(bookInReader);
-                return searchedBook;
+        for (Book bookInCatalog : read) {
+            if ((title).equals(bookInCatalog.getTitle())){
+                searchedBook.add(bookInCatalog);
             }
         }
         return searchedBook;
@@ -43,14 +38,12 @@ public class Reader implements Search {
     @Override
     public List<Book> searchByAuthor(Author author) {
         List<Book> searchedBook = new ArrayList<>();
-        while (readerIterator.hasNext()) {
-            Book bookInCatalog = (Book) readerIterator.next();
-            if (bookInCatalog.getAuthor().getName().equals(author.getName())) {
+        for (Book bookInCatalog : read) {
+            if ((author.getName()).equals(bookInCatalog.getAuthor().getName())){
                 searchedBook.add(bookInCatalog);
-                return searchedBook;
             }
         }
         return searchedBook;
-
-
+    }
 }
+
