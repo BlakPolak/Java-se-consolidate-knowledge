057f46d3cf4a062d2f08177e9205c830b97bf5c8
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-10 16:22:37 +0200

    Add implements for Search interface

--- src/main/java/com.codecool.krk20161.javase.model/Catalog.java
+++ src/main/java/com.codecool.krk20161.javase.model/Catalog.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class Catalog {
+public class Catalog implements Search{
 
     public ArrayList<Book> books;
     Iterator catalogIterator;
@@ -35,6 +35,10 @@ public class Catalog {
         }
     }
 
+    public void remove(Book bookToRemove) {
+    }
+
+    @Override
     public List<Book> searchByTitle(String bookTittle) {
         List<Book> searchedBook = new ArrayList<>();
         while (catalogIterator.hasNext()) {
@@ -47,12 +51,10 @@ public class Catalog {
         return searchedBook;
     }
 
+
+    @Override
     public List<Book> searchByAuthor(Author author) {
         return null;
-    }
-
-
-    public void remove(Book bookToRemove) {
     }
 
 
