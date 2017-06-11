5ffb0c29670322877c4beb0304550a2345a1aadc
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-11 21:05:10 +0200

    Add empty lines

--- src/main/java/com/codecool/krk20161/javase/model/Library.java
+++ src/main/java/com/codecool/krk20161/javase/model/Library.java
@@ -4,10 +4,12 @@ package com.codecool.krk20161.javase.model;
 import com.codecool.krk20161.javase.controller.Catalog;
 
 public class Library {
+
     Catalog catalog = new Catalog();
 
     public Library(){
     }
+
     public Catalog getCatalog() {
         return catalog;
     }
