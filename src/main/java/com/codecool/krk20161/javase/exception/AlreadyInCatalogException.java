44e761fe84477ba0f3eb153824ac0d463edeb50b
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-11 20:53:24 +0200

    Add empty line

--- src/main/java/com/codecool/krk20161/javase/exception/AlreadyInCatalogException.java
+++ src/main/java/com/codecool/krk20161/javase/exception/AlreadyInCatalogException.java
@@ -2,6 +2,7 @@ package com.codecool.krk20161.javase.exception;
 
 
 public class AlreadyInCatalogException extends Exception {
+
     public AlreadyInCatalogException(String message){
         super(message);
     }
