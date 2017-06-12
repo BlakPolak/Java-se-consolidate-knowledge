68af3e7cd5cfb7d40b200253cf86fbccec7d1d8c
Pawel Polakiewicz <pb.polakiewicz@gmail.com>
2017-06-12 15:47:32 +0200

    Add constructor and getName to Person class

---
+++ src/main/java/com/codecool/krk20161/javase/model/Person.java
@@ -0,0 +1,18 @@
+package com.codecool.krk20161.javase.model;
+
+/**
+ * Created by ppolak on 12.06.17.
+ */
+public class Person {
+
+    private final String name;
+
+    public Person(String name){
+        this.name = name;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
